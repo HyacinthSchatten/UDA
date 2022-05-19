7e5ecc9daff5cf569ca0a0e271c27481e1fa3767
Arisu Kirisame <56330066+HyacinthSchatten@users.noreply.github.com>
2022-05-20 02:17:20 +0800

    Update tsne.py

--- tsne.py
+++ tsne.py
@@ -4,3 +4,35 @@ import pandas as pd
 import seaborn as sns
 from sklearn import datasets
 from sklearn import manifold
+import dataloader
+
+
+data = dataloader()
+# data returns a tuple (features, target)
+features, target = data
+target = target.astype(int)
+
+# dimensionality reduction using t-SNE
+tsne = manifold.TSNE(n_components=2, random_state=42)
+# fit and transform
+mnist_tr = tsne.fit_transform(features[:30000])
+# transformed_data is a 2D numpy array of shape (30000, 2)
+
+# create dataframe
+cps_df = pd.DataFrame(columns=['CP1', 'CP2', 'target'],
+                       data=np.column_stack((mnist_tr, 
+                                            target.iloc[:30000])))
+# cast targets column to int
+cps_df.loc[:, 'target'] = cps_df.target.astype(int)
+cps_df.head()
+
+# data related
+domain_map = {0:'T-shirt/top'}
+
+# map targets to actual clothes for plotting
+cps_df.loc[:, 'target'] = cps_df.target.map(domain_map)
+
+cps_df.target.value_counts().plot(kind='bar')
+
+grid = sns.FacetGrid(cps_df, hue="target", size=8)
+grid.map(plt.scatter, 'CP1', 'CP2').add_legend()
